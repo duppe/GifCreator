7321e89e06b7dd8700674a5483e8f5c6b714db50
阮振伟 <rwecho@live.com>
2016-01-11 15:17:37 +0800

    增加异常处理。

--- GifCreator/App.xaml.cs
+++ GifCreator/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows;
 
 namespace GifCreator
 {
@@ -9,6 +12,57 @@ namespace GifCreator
     {
         public App()
         {
+
+            this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+
+            this.Dispatcher.UnhandledException += Dispatcher_UnhandledException;
+
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+
+        }
+
+        private void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            Debug.WriteLine(e.Exception);
+            e.SetObserved();
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            //e.ExceptionObject;
+
+            Debug.WriteLine(e.ExceptionObject);
+
+
+
+#if !DEBUG
+            //程序即将退出。
+            Environment.Exit(-1);
+#endif
+        }
+
+        private void Dispatcher_UnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            if (!e.Handled)
+            {
+                //在App_DispatcherUnhandledException未处理的异常
+                WpfInfras.Windows.ExceptionViewer exViewer = new WpfInfras.Windows.ExceptionViewer(e.Exception);
+
+                var handled = exViewer.ShowDialog() == true;
+
+                e.Handled = handled;
+            }
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            WpfInfras.Windows.ExceptionViewer exViewer = new WpfInfras.Windows.ExceptionViewer(e.Exception);
+
+            var handled = exViewer.ShowDialog() == true;
+
+            e.Handled = handled;
         }
     }
 }
